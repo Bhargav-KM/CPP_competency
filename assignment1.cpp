86108eedaee2f723dc01c180aa238ada69572e25
Bhargav K M <68016352+Bhargav-KM@users.noreply.github.com>
2023-05-18 01:50:28 +0530

    assignment 1

---
+++ assignment1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Car{
+	private:
+    	int ReleaseYear;
+        string customer;
+        int speed;
+	
+    public:
+    	Car(int Year, string Customer, int Speed){    //constructor
+        	ReleaseYear = Year;
+            customer = Customer;
+            speed = Speed;
+        }
+        
+        int get_ReleaseYear(){
+        	return ReleaseYear;
+        }
+        
+        string get_customer(){
+        	return customer;
+        }
+        
+        int get_speed(){
+        	return speed;
+        }
+        
+        void accelerate(){
+        	speed += 10;
+        }
+        
+        void brake(){
+        	speed -= 10;
+        }
+
+};
+
+
+int main() {
+	Car car1(2000, "Tata", 0);
+    
+    //accelerate 5 times
+    for(int i=0; i<5; i++){
+    	car1.accelerate();
+    }
+    cout << "Car1 speed after accl 5 times = " << car1.get_speed() <<endl;
+    
+    //brake 5 times
+    for(int i=0; i<5; i++){
+    	car1.brake();
+    }
+    cout << "Car1 speed after brake 5 times = " << car1.get_speed();
+    
+	return 0;
+}
